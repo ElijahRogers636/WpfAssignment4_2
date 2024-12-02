f4e195dab2f264cce9b1d921ff9d193c79234430
Elijah Rogers <ejrogersemail@gmail.com>
2024-12-02 09:02:44 -0500

    Add project files.

---
+++ Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAssignment4_2
+{
+    public class Student : SchoolPerson
+    {
+        public string Grade { get; set; }
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAssignment4_2
+{
+    public class Teacher : SchoolPerson
+    {
+        // List of students
+        List<Student> students;
+
+        // Holds class and the students in the class
+        Dictionary<string, List<Student>> studentsDictionary;
+    }
+}
